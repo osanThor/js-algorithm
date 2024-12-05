f4bc761ea89b0cfd7d925675f822211b19fa46cc
junyoung <jadw9715@gmail.com>
2024-12-06 00:45:07 +0900

    feat. sec6-1

---
+++ sec6/index.js
@@ -0,0 +1,14 @@
+// 1
+// function solution(str) {
+//   let count = 0;
+//   for (let x of str) {
+//     if (x === "(") count++;
+//     else if (x === ")") count--;
+//   }
+//   return count === 0 ? "YES" : "NO";
+// }
+// console.log(solution("(())()"));
+
+// 2
+function solution(str) {}
+console.log(solution("(A(BC)D)EF(G(H)(IJ)K)LM(N)"));
