947e41786f79954977f85051c7263850caf9cbe9
junyoung <jadw9715@gmail.com>
2024-12-18 01:16:51 +0900

    add. sec6 -2

--- sec6/index.js
+++ sec6/index.js
@@ -10,5 +10,25 @@
 // console.log(solution("(())()"));
 
 // 2
-function solution(str) {}
+function solution(str) {
+  let answer,
+    stack = [];
+  for (let x of str) {
+    if (x === ")") {
+      while (stack.pop() !== "(");
+    } else stack.push(x);
+  }
+  answer = stack.join("");
+  return answer;
+}
+// function solution(str) {
+//   let answer = "",
+//     count = 0;
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] === "(") count++;
+//     else if (str[i] === ")") count--;
+//     if (count === 0 && str[i] !== ")") answer += str[i];
+//   }
+//   return answer;
+// }
 console.log(solution("(A(BC)D)EF(G(H)(IJ)K)LM(N)"));
